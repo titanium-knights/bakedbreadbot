b867c4a063018c6eb79baa2c7387e36f3961a724
Anthony Li <me@anli.dev>
2022-02-17 09:44:40 -0500

    Add telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.kt
@@ -1,13 +1,17 @@
 package org.firstinspires.ftc.teamcode
 
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.config.Config
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.util.ToggleButton
 
 @TeleOp(name = "Baked Bread Bot")
-class BreadBotTeleOpMode: LinearOpMode() {
+@Config class BreadBotTeleOpMode: LinearOpMode() {
     override fun runOpMode() {
         val drive = MecanumDrive(hardwareMap)
+        val telemetry = MultipleTelemetry(telemetry, FtcDashboard.getInstance().telemetry)
 
         val spinButton = ToggleButton { gamepad1.x }
         val fastButton = ToggleButton { gamepad1.y }
@@ -17,12 +21,29 @@ class BreadBotTeleOpMode: LinearOpMode() {
         while (opModeIsActive()) {
             if (spinButton.isActive) {
                 drive.move(0.0, 0.0, 0.2)
+                telemetry.addData("Spinning", "Yes")
             } else {
-                drive.teleOpRobotCentric(gamepad1, if (fastButton.isActive) 1.0 else 0.4)
+                drive.teleOpRobotCentric(gamepad1, if (fastButton.isActive && ENABLE_FAST_MODE) 1.0 else 0.4)
+                telemetry.addData("Spinning", "No")
             }
+
+            telemetry.addData("Fast Mode", when {
+                !ENABLE_FAST_MODE -> "Disabled"
+                fastButton.isActive -> "Yes"
+                else -> "No"
+            })
+
+            telemetry.addData("X", gamepad1.left_stick_x)
+            telemetry.addData("Y", -gamepad1.left_stick_y)
+            telemetry.addData("Rot", gamepad1.right_stick_x)
 
             spinButton.update()
             fastButton.update()
+            telemetry.update()
         }
+    }
+
+    companion object {
+        var ENABLE_FAST_MODE = true
     }
 }
